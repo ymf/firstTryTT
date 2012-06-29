aa353f79610c0e8ba350012cadd8bab53f954c4d
YMF <yesmehfriend@gmail.com>
2012-06-29 21:32:28 +0200

    Initial commit gogo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstTryTT::Application.config.secret_token = '3985e1dca6e497ef787e12b8b6b057112102ffc75fdaaa1e8a66675164fa5d54b4ef9a123035596923494cfe9d96fda7de739f88601682dc5085f4b2b90f02ec'
